d106e7cd83786d2db2c36687024ea6e217123dfd
jahann <vhanj@telenet.be>
2014-06-10 21:51:29 +0200

    first version of assignment
    first version tested using R-studio. This should be extended to check if
    the inversed matrix corresponds to the input matrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        mtx <- NULL
+        set <- function(y) {
+                x <<- y
+                mtx <<- NULL
+        }
+        get <- function() x
+        setinversematrix <- function(solve) mtx <<- solve
+        getinversematrix <- function() mtx
+        list(set = set, get = get,
+             setinversematrix = setinversematrix,
+             getinversematrix = getinversematrix)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        mtx <- x$getinversematrix()
+        if(!is.null(mtx)) {
+                message("getting cached inversed matrix")
+                return(mtx)
+        }
+        data <- x$get()
+        mtx <- solve(data, ...)
+        x$setinversematrix(mtx)
+        mtx      ## Return a matrix that is the inverse of 'x'
 }
